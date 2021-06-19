bc9fd17e0e20a19d3375e34686686e5d64559e15
FrostDragonGames <43161576+Jman1868@users.noreply.github.com>
2021-06-18 23:21:48 -0700

    Updated the documentation and removed some old test lines

--- JS/main.js
+++ JS/main.js
@@ -1,41 +1,52 @@
 
+//Variables so we can determine the time
 var min=9;
 var sec=59;
-var counter=0;
 
+var counter=0; //Used to switch back and forth 
+
+//Refrence to our html
 const button = document.querySelector('href')
 const mintext = document.getElementById("min");
 const sectext = document.getElementById("sec");
 const breathtext = document.getElementById("breath");
 
-///TODO: DOCUMENT THIS CODE
-function timer()
+//The Timer function calls set interval function for timedo and
+//textinstrcut. It also Intialzies the text.
+function timer(link)
 {
    
-    console.log("help HELP"); 
     timedo = setInterval(calcsec, 1000);
     breathtext.textContent = "Breath in";
     textinstruct = setInterval(changeText, 5000);
+    link.onclick = function (event) {
+        event.preventDefault();
+    }
 }
 
+//Change text changes the text by switching on each set interval call.
 function changeText()
 {
     if(counter==0)
     {
         breathtext.textContent="Breath out";
         counter=1;
-    }else if(counter==1)
+    }
+    else if(counter==1)
     {
         breathtext.textContent = "Breath in";
         counter=0;
     }
 }
 
+
+//Calcsec, calcualates the second that is left. If seconds is greater than zero
+//we subtract by 1. Otherwise we move on the the Calc Min.
 function calcsec()
 {
     sectext.textContent = sec;
     mintext.textContent=min;
-    console.log(sec);
+
     if(sec>0)
     {
         sec -= 1;
@@ -48,6 +59,12 @@ function calcsec()
  
 }
 
+//Caclcmin Calculates the minutes that are left.
+//If the min value is greater than zero we reset the seconds
+//counter value and subtract by one.
+//If min value ever reaches zero, then we know our timer
+//is over. Once we know that we stop all the intervals
+// and exit out.
 function calcMin()
 {
     console.log("min is currently: "+min);
